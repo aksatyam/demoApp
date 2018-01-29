873cc23f0658d2e148994c8534fd2ec90c750de8
aksatyam <ashishsatyam.kmr@gmail.com>
2018-01-29 15:52:52 +0530

    second commit

---
+++ app/src/main/java/com/example/muhammadaarif/examplegooglemap/demo_activity.java
@@ -0,0 +1,13 @@
+package com.example.muhammadaarif.examplegooglemap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class demo_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_activity);
+    }
+}
